04bcc576198026f9e831a733f6062401f748cc44
J4IR192000 <patitofeliz2000@hotmail.com>
2024-09-19 01:46:19 -0500

    Interfaz

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    private static void prinln(String hola) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Scanner reader = new Scanner(System.in);
+//        int n1=10,n2,r;
+//        System.out.println("Escribe el valor de n1");
+//        n1 = reader.nextInt();
+//        System.out.println("n1 vale: "+n1);
+//        System.out.println("Escribelo de nuevo");
+//        n1 = reader.nextInt();
+//        System.out.println("n1 vale: "+n1);
+//        System.out.println("Escribe otro numero");
+//        n2 = reader.nextInt();
+//        r=n1+n2;
+//        System.out.println("La suma da como resultado: "+r);
+        Interfaz obj = new Interfaz();
+        obj.setVisible(true);
+    }
+    
+}
